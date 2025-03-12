d75e2a4f95178f1f3b68d446db1914e381ee1810
Luis <luis.wiedmann@gmail.com>
2025-03-12 14:30:04 +0100

    fixed notebook and moved functions into py file

--- energy.py
+++ energy.py
@@ -1,5 +1,6 @@
 from typing import List
 from rna_structure import RNAStructure
+import RNA
 
 def base_pair_minimization_energy(structure: RNAStructure) -> float:
     """
@@ -13,3 +14,15 @@ def base_pair_minimization_energy(structure: RNAStructure) -> float:
         The negative number of base pairs as a float value (more negative = more stable)
     """
     return -len(structure.base_pairs)
+
+def turner_energy(fold_compound: RNA.RNA.fold_compound, structure: str) -> float:
+    """Calculates the Turner Energy of a Structure using the ViennaRNA Package
+
+    Args:
+        fold_compound (RNA.RNA.fold_compound): fold compound for an RNA sequence, e.g.: fold_compound = RNA.fold_compound("GCGCGAUACG")
+        structure (str): test structure (dot-bracket notation), e.g.: structure = "((....)).."
+
+    Returns:
+        float: Turner energy of this structure
+    """
+    return fold_compound.eval_structure(structure)
