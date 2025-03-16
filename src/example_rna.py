9a624a715b21b22cf8bf34deed6c1cba74eac454
GuigzRoll <guillemette.rolland@polytechnique.edu>
2025-03-16 23:35:09 +0100

    +3 examples

--- src/example_rna.py
+++ src/example_rna.py
@@ -11,3 +11,14 @@ MED_SEQ = "GGAAGCGUAUCGUCACCGGUGUGGCGCCCGGUCUUCAAAACCGGUGGACGGCUGAGAGGUCGUCGGUAG
 MED_END_STRUCT = "(((((((((.((((((....))))))((((((.......)))))).(((((........)))))..((((.......)))))))))))))"
 MED_START_STRUCT = "((((((((((...(((((((..(((.....)))......)))))))(((((((....)))))))((.((......)))).))))))))))"
 
+VLONG_SEQ = "GGCUGGUCCGAAGGUAGUGAGUUAUCUCAAUUGAUUGUUCACAGUCAGUUACAGAUUGAACUCCUUGUUCUACUCUUUCCCCCCUUCUCACUACUGCACUUGACUAGUCUUUU"
+VLONG_START_STRUCT = "((((((((.((..((((((((.....................(((.((.....................)).)))...........))))))...))..))))))))))...."
+VLONG_END_STRUCT = "((((((((....(((((((((..((((.((((((((((..))))))))))..)))).((((.....))))................)))))))))......))))))))...."
+
+ANOTHER_SEQ = "GGGCCCAUAGCUCAGUGGUAGAGUGCCUCCUUUGCAAGGAGGAUGCCCUGGGUUCGAAUCCCAGUGGGUCCA"
+ANOTHER_START_STRUCT = "(((((((..((((.......)))).(((((.......))))).....(((((.......))))))))))))."
+ANOTHER_END_STRUCT = "(((((((((((((((.((((.....(((((((...))))))).))))))))))).........))))))))."
+
+MOD_SEQ = "GCCAUCCCAGUGUUCAGACUACCUGUUCAGGAGGCUGGGACAUGUACAGUAGUCUGCACAUUGGUUAGGC"
+MOD_START_STRUCT = "......(((((((.(((((((.((((.((.............)).))))))))))).)))))))......"
+MOD_END_STRUCT = "(((...(((((((.((((((((.(((((((....)))).......))))))))))).)))))))...)))"
