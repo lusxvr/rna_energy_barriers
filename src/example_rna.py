69ee26d5f72f2ebc8856817e11bc6edf15b1e1ae
GuigzRoll <guillemette.rolland@polytechnique.edu>
2025-03-16 19:29:57 +0100

    modified:   src/example_rna.py

--- src/example_rna.py
+++ src/example_rna.py
@@ -5,3 +5,9 @@ SHORT_END_STRUCT = "...((...))."
 LONG_SEQ = "ACACGACCUCAUAUAAUCUUGGGAAUAUGGCCCAUAAGUUUCUACCCGGCAACCGUAAAUUGCCGGACUAUGCAGGGAAGUG"
 LONG_START_STRUCT = "......................(....................)....(.............)..................."
 LONG_END_STRUCT = ".(((..(((((((......((((.......))))...........(((((((.......)))))))..)))).)))...)))"  # target structure
+
+
+MED_SEQ = "GGAAGCGUAUCGUCACCGGUGUGGCGCCCGGUCUUCAAAACCGGUGGACGGCUGAGAGGUCGUCGGUAGGUUCGACUCCUAUACGCUUCC"
+MED_END_STRUCT = "(((((((((.((((((....))))))((((((.......)))))).(((((........)))))..((((.......)))))))))))))"
+MED_START_STRUCT = "((((((((((...(((((((..(((.....)))......)))))))(((((((....)))))))((.((......)))).))))))))))"
+
